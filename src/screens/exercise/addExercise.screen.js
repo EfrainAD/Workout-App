fe263d8f9aa691f69e7eb94acbdf5f0bd9af3225
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-12-07 11:02:24 -0500

    WA-95 Add the rest of the fields that the AddExercise form will need

--- src/screens/exercise/addExercise.screen.js
+++ src/screens/exercise/addExercise.screen.js
@@ -1,4 +1,4 @@
-import { Text, View, TextInput, Button, Alert } from 'react-native'
+import { Text, View, TextInput, Button, Alert, ScrollView } from 'react-native'
 import { useForm, Controller } from 'react-hook-form'
 import { useEffect, useState } from 'react'
 import { theme } from '../../infrastructure/theme'
@@ -9,9 +9,10 @@ import {
    dropExerciseTable,
    getExercises,
 } from '../../services/api/exersice'
+import { CenteredView } from '../../infrastructure/styledComponents/styledComonents'
 
 const AddExercise = () => {
-   const [lists, setLists] = useState()
+   const [lists, setLists] = useState([])
    const {
       control,
       handleSubmit,
@@ -22,6 +23,10 @@ const AddExercise = () => {
          name: '',
          level: '',
          description: '',
+         sets_goal: null, // int
+         reps_goal: '',
+         duration_goal: '', // interval - format DD:HH:MM:SS
+         archive: false,
       },
    })
    const onSubmit = (data) => {
@@ -37,80 +42,171 @@ const AddExercise = () => {
    }, [])
 
    return (
-      <SafeArea style={{ flex: 1, alignItems: 'center' }}>
-         <View style={{ padding: 30 }}>
-            <Text style={{ fontSize: theme.fontSizes.h2 }}>Workouts!</Text>
-         </View>
-         <View>
-            {lists &&
-               lists.map((list, idx) => (
-                  <Text key={idx}>
-                     {list.name} {list.level} {list.description}
+      <SafeArea>
+         <ScrollView>
+            <CenteredView>
+               <View style={{ padding: 30 }}>
+                  <Text style={{ fontSize: theme.fontSizes.h2 }}>
+                     Workouts!
                   </Text>
-               ))}
+               </View>
+               <View>
+                  {/* DISPLAY DATA */}
+                  {lists.map((list, idx) => (
+                     <Text key={idx}>
+                        Name: {list.name} {'\n'}
+                        Level: {list.level} {'\n'}
+                        Description: {list.description} {'\n'}
+                        Sets Goal: {list.sets_goal} {'\n'}
+                        Reps Goal: {list.reps_goal} {'\n'}
+                        Duration Goal: {list.duration_goal} {'\n'}
+                        Archive: {list.archive ? 'Yes' : 'No'} {'\n'}
+                     </Text>
+                  ))}
 
-            <Controller
-               control={control}
-               rules={{
-                  required: true,
-               }}
-               render={({ field: { onChange, onBlur, value } }) => (
-                  <TextInput
-                     style={{ fontSize: '60px' }}
-                     placeholder="Name"
-                     onBlur={onBlur}
-                     onChangeText={onChange}
-                     value={value}
+                  {/* FORM */}
+
+                  {/*  Name */}
+                  <Controller
+                     control={control}
+                     rules={{
+                        required: true,
+                     }}
+                     render={({ field: { onChange, onBlur, value } }) => (
+                        <TextInput
+                           style={{
+                              fontSize: parseInt(
+                                 theme.fontSizes.body.slice(0, -2)
+                              ),
+                           }}
+                           placeholder="Name"
+                           onBlur={onBlur}
+                           onChangeText={onChange}
+                           value={value}
+                        />
+                     )}
+                     name="name"
                   />
-               )}
-               name="name"
-            />
-            {errors.name && <Text>This is required.</Text>}
+                  {errors.name && <Text>This is required.</Text>}
 
-            <Controller
-               control={control}
-               rules={{
-                  maxLength: 100,
-               }}
-               render={({ field: { onChange, onBlur, value } }) => (
-                  <TextInput
-                     style={{ fontSize: '60px' }}
-                     placeholder="Level"
-                     onBlur={onBlur}
-                     onChangeText={onChange}
-                     value={value}
+                  {/* Level */}
+                  <Controller
+                     control={control}
+                     rules={{
+                        maxLength: 100,
+                     }}
+                     render={({ field: { onChange, onBlur, value } }) => (
+                        <TextInput
+                           style={{
+                              fontSize: parseInt(
+                                 theme.fontSizes.body.slice(0, -2)
+                              ),
+                           }}
+                           placeholder="Level"
+                           onBlur={onBlur}
+                           onChangeText={onChange}
+                           value={value}
+                        />
+                     )}
+                     name="level"
                   />
-               )}
-               name="level"
-            />
 
-            <Controller
-               control={control}
-               // rules={{
-               //    maxLength: 100,
-               // }}
-               render={({ field: { onChange, onBlur, value } }) => (
-                  <TextInput
-                     style={{ fontSize: '60px' }}
-                     placeholder="Description"
-                     onBlur={onBlur}
-                     onChangeText={onChange}
-                     value={value}
+                  {/* Sets Goal */}
+                  <Controller
+                     control={control}
+                     rules={{
+                        required: true,
+                        pattern: /^[0-9]*$/,
+                     }}
+                     render={({ field: { onChange, onBlur, value } }) => (
+                        <TextInput
+                           style={{
+                              fontSize: parseInt(
+                                 theme.fontSizes.body.slice(0, -2)
+                              ),
+                           }}
+                           placeholder="Sets Goal"
+                           keyboardType="numeric"
+                           onBlur={onBlur}
+                           onChangeText={onChange}
+                           value={value}
+                        />
+                     )}
+                     name="sets_goal"
                   />
-               )}
-               name="description"
-            />
+                  {errors.sets_goal && (
+                     <Text>Only numeric input is allowed for Sets Goal.</Text>
+                  )}
 
-            <Button title="Submit" onPress={handleSubmit(onSubmit)} />
-         </View>
+                  {/* Reps Goal */}
+                  <Controller
+                     control={control}
+                     render={({ field: { onChange, onBlur, value } }) => (
+                        <TextInput
+                           style={{
+                              fontSize: parseInt(
+                                 theme.fontSizes.body.slice(0, -2)
+                              ),
+                           }}
+                           placeholder="Reps Goal"
+                           onBlur={onBlur}
+                           onChangeText={onChange}
+                           value={value}
+                        />
+                     )}
+                     name="reps_goal"
+                  />
 
-         <Button
-            title="DROP DB TABLE"
-            onPress={() => {
-               dropExerciseTable()
-               setLists(null)
-            }}
-         />
+                  {/* Duration Goal  */}
+                  <Controller
+                     control={control}
+                     render={({ field: { onChange, onBlur, value } }) => (
+                        <TextInput
+                           style={{
+                              fontSize: parseInt(
+                                 theme.fontSizes.body.slice(0, -2)
+                              ),
+                           }}
+                           placeholder="Duration Goal (DD:HH:MM:SS)"
+                           onBlur={onBlur}
+                           onChangeText={onChange}
+                           value={value}
+                        />
+                     )}
+                     name="duration_goal"
+                  />
+
+                  {/* Description */}
+                  <Controller
+                     control={control}
+                     render={({ field: { onChange, onBlur, value } }) => (
+                        <TextInput
+                           style={{
+                              fontSize: parseInt(
+                                 theme.fontSizes.body.slice(0, -2)
+                              ),
+                           }}
+                           placeholder="Description"
+                           onBlur={onBlur}
+                           onChangeText={onChange}
+                           value={value}
+                        />
+                     )}
+                     name="description"
+                  />
+
+                  <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+               </View>
+
+               <Button
+                  title="DROP DB TABLE"
+                  onPress={() => {
+                     dropExerciseTable()
+                     setLists([])
+                  }}
+               />
+            </CenteredView>
+         </ScrollView>
       </SafeArea>
    )
 }
